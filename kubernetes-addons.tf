53ae362440cbe97a857be72c8c66a020d8de7c03
dbaltor <39310315+dbaltor@users.noreply.github.com>
2024-04-06 13:12:57 +0100

    upgrade eks-blue-prints-addons

--- kubernetes-addons.tf
+++ kubernetes-addons.tf
@@ -1,6 +1,6 @@
 module "eks_blueprints_addons" {
   source = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.16.0"
+  version = "1.16.2"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
