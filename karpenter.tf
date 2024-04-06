b4e28ba52db7180990ab733002aff37c084dce56
dbaltor <39310315+dbaltor@users.noreply.github.com>
2024-04-06 19:46:08 +0100

    upgrade karpenter to 0.35.4

--- karpenter.tf
+++ karpenter.tf
@@ -22,7 +22,7 @@ resource "helm_release" "karpenter" {
   name                = "karpenter"
   repository          = "oci://public.ecr.aws/karpenter"
   chart               = "karpenter"
-  version             = "v0.34.0"
+  version             = "0.35.4"
 
   set {
     name  = "settings.clusterName"
